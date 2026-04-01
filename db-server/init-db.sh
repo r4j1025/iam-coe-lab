f7d19337932280f67bf0e7910f3108658ad93531
Luke George <luke@Lukes-MacBook.local>
2026-04-01 11:01:05 +0530

    Updated the conf for Alpha Apps

--- db-server/init-db.sh
+++ db-server/init-db.sh
@@ -9,36 +9,26 @@ if [ -f .env ]; then
   source .env
 fi
 
-TYPE=${1:-${DB_TYPE:-mysql}}
+CONTAINER_NAME=${MYSQL_CONTAINER_NAME:-mysql_db}
+SQL_DIR="$ROOT_DIR/conf"
 
-case "$TYPE" in
-  mysql)
-    CONTAINER_NAME=${MYSQL_CONTAINER_NAME:-mysql_db}
-    echo "Initializing MySQL inside container: $CONTAINER_NAME"
-    docker exec -i "$CONTAINER_NAME" mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<SQL
-CREATE DATABASE IF NOT EXISTS \\`${MYSQL_DATABASE}\\`;
+echo "Initializing MySQL inside container: $CONTAINER_NAME"
+docker exec -i "$CONTAINER_NAME" mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<SQL
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \\`${MYSQL_DATABASE}\\`.* TO '${MYSQL_USER}'@'%';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 SQL
-    ;;
-  postgres)
-    CONTAINER_NAME=${POSTGRES_CONTAINER_NAME:-postgres_db}
-    echo "Initializing Postgres inside container: $CONTAINER_NAME"
-    docker exec -i "$CONTAINER_NAME" psql -U "${POSTGRES_USER}" -d "postgres" <<SQL
-DO
-\$\$BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${POSTGRES_DB}') THEN
-      PERFORM pg_database_create('${POSTGRES_DB}');
-   END IF;
-END
-\$\$;
-CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';
-GRANT ALL PRIVILEGES ON DATABASE "${POSTGRES_DB}" TO ${POSTGRES_USER};
-SQL
-    ;;
-  *)
-    echo "Unsupported DB type: $TYPE" >&2
-    exit 2
-    ;;
-esac
+
+shopt -s nullglob
+sql_files=("$SQL_DIR"/*.sql)
+if [ "${#sql_files[@]}" -gt 0 ]; then
+  echo "Applying SQL files from $SQL_DIR"
+  for sql_file in "${sql_files[@]}"; do
+    echo "Running $(basename "$sql_file")"
+    docker exec -i "$CONTAINER_NAME" mysql -u root -p"${MYSQL_ROOT_PASSWORD}" "${MYSQL_DATABASE}" < "$sql_file"
+  done
+else
+  echo "No SQL files found in $SQL_DIR"
+fi
+shopt -u nullglob
